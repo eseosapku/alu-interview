79110c7e680fe9f0d3232683410001d9ea3d01a2
eseosapku <e.uwagboe@alustudent.com>
2023-02-10 21:28:45 +0000

    commit

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""
-Minimum Operation"""
+"""Minimum Operation"""
 def minOperations(n):
     """get minimum double , and one
     operations
     """
 
 
-    operations = 0
-    characters = 2
+    x = 0
+    char = 2
     while n > 1:
-        while n % characters == 0:
-            operations += characters
-            n = n / characters
-        characters += 1
-    return operations
+        while n % char == 0:
+            x += char
+            n = n / char
+        char += 1
+    return x
