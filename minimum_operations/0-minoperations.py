16370e2dd9039cface8d365babf26a787aaafcb2
eseosapku <e.uwagboe@alustudent.com>
2023-02-10 21:15:30 +0000

    commit

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 """
 Minimum Operation"""
-
 def minOperations(n):
     """Return 0 if n is less than or equal to 0"""
     if n <= 0:
         return 0
-
     operations = 0 
-
-    for t in range(2, n/2):
-        while(n % t == 0):
+    for x in range(2, n/2):
+        while(n % x == 0):
             operations += t 
             n = n / t
     return(operations)
