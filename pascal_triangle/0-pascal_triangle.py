cb0d3c9bfc00bc0848e1b477f1ffdc3e628ab740
eseosapku <e.uwagboe@alustudent.com>
2023-03-24 19:46:11 +0000

    commit

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -11,8 +11,8 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     pascal = [[1]]
-    for number in range(n-1):
+    for item in range(n-1):
         pascal.append(
-            [a+b for a, b in zip([0] + pascal[-1], pascal[-1] + [0])])
+            [i+j for i, j in zip([0] + pascal[-1], pascal[-1] + [0])])
 
-    return pascalpp.run(host="0.0.0.0", port=5000, debug=True)
+    return pascal
